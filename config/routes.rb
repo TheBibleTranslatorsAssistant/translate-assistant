54692390967f5a558bd0abde16b7db871812f1fa
Jason Ardell <ardell@gmail.com>
2015-11-07 14:50:50 -0700

    Add json route for concepts#index with query param q.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :words, only: [:index], defaults: { format: 'json' }
+  resources :concepts, only: [:index], defaults: { format: 'json' }
 
   # Example resource route with options:
   #   resources :products do
