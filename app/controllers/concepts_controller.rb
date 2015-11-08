dab0dc58c85adba40ccf9f3e328201198d6f63f9
Jason Ardell <ardell@gmail.com>
2015-11-07 19:14:36 -0700

    Filter out period characters when searching for concepts.

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -3,7 +3,8 @@ class ConceptsController < ApplicationController
   def index
     query = Concept.order(title: :asc, description: :asc).limit(10)
     if params[:q]
-      query_with_wildcards = "%#{params[:q]}%"
+      # Nasty hack: strip periods in order to match DB records
+      query_with_wildcards = "%#{params[:q].gsub(/\./, '')}%"
       query = query.where(
         'title ILIKE ? OR description ILIKE ?',
         query_with_wildcards,
