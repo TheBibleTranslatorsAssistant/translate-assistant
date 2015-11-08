6a19cb8153811355b2a5825b2258040017f784f6
nsackman <sackman.nicholas@gmail.com>
2015-11-07 17:18:22 -0700

    refactored the jumbotron with conditional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    render :index, layout: 'jumbotron'
   end
 
   def about
-  	render :about, layout: 'jumbotron'
   end
 
   def how_it_works
-  	render :how_it_works, layout: 'jumbotron'
   end
 
   def translate
